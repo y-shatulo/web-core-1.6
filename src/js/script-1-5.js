2d8c9f4ad4c6d017f0e51856140bab453f0149a8
Yuliya Shatulo <yulya915@mail.ru>
2023-01-25 23:26:37 +0300

    all ready

--- src/js/script-1-5.js
+++ src/js/script-1-5.js
@@ -1,29 +1,20 @@
-export let slider = document.querySelector('.swiper')
-export let mySwiper
-export function mobileSlider() {
-  if (window.innerWidth <= 767 && slider.dataset.mobile === 'false') {
-    mySwiper = new Swiper(slider, {
+import Swiper, { Pagination } from 'swiper';
+
+
+const sliders = document.querySelectorAll('.swiper');
+
+sliders.forEach((el) => {
+	  let swiper = new Swiper(el, {
+        modules: [Pagination],
       spaceBetween: 16,
       slidesPerView: 'auto',
-
       pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-      }
-    })
-    slider.dataset.mobile = 'true'
-  }
+          el: '.swiper-pagination',
+          type: 'bullets',
+          clickable: true,
+      },
+	});
+});
 
-  if (window.innerWidth >= 768) {
-    slider.dataset.mobile = 'false'
-    if (slider.classList.contains('swiper-initialized')) {
-      mySwiper.destroy()
-    }
-  }
-}
 
-mobileSlider()
 
-window.addEventListener('resize', () => {
-  mobileSlider()
-})
